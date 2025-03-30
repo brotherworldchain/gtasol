7fb5df8f31e6d9de9f8bb77e72fe968af067fa56
StephenChi-hi <nwikechisom77@gmail.com>
2025-03-30 11:51:08 +0100

    sd

--- app/head.js
+++ app/head.js
@@ -1,6 +1,6 @@
 const Head = () => (
   <>
-    <title>Meeka The Husky – The Sassiest Dog on Solana!</title>
+    <title>GTA 6 Token</title>
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <link rel="icon" href="/favicon.ico" />
   </>
